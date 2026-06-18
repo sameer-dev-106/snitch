2cdc2bfba57390300a4c621c10d4b1a2610c596e
Sameer <sk6586418@gmail.com>
2026-06-18 18:22:39 +0530

    feat: implement API service for user registration with error handling

---
+++ Frontend/src/features/service/auth.api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const API = axios.create({
+    baseURL: `${BACKEND_URL}/api/auth`,
+    withCredentials: true,
+});
+
+const handleApiError = (err, fallbackMessage) => {
+    throw (
+        err?.response?.data?.message ||
+        err?.message ||
+        fallbackMessage
+    );
+}
+
+export const registerApi = async({ email, contact, password, fullname, isSeller }) => {
+    try {
+        const response = await API.post("/register", { email, contact, password, fullname, isSeller });
+        return response.data;
+    } catch (err) {
+        handleApiError(err, "Failed to register user.");
+    }
+}
