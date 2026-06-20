6ce6f8402f68649e3879f8a7abb49224c0ba3356
Sameer <sameer.dev106@gmail.com>
2026-06-20 08:15:55 +0530

    feat: create Register component for user registration page

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,19 @@
+import LeftSide from "../components/LeftSide";
+import RightSide from "../components/RightSide";
+
+const Register = () => {
+  return (
+      <main
+        className="min-h-screen flex flex-col lg:flex-row selection:bg-[#C9A96E]/30"
+        style={{
+          backgroundColor: "#fbf9f6",
+          fontFamily: "'Inter', sans-serif",
+        }}
+      >
+        <LeftSide />
+        <RightSide />
+      </main>
+  );
+};
+
+export default Register;
