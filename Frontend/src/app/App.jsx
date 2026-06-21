51ea4538f00a4588319d9234f4570795ae228901
Sameer <sameer.dev106@gmail.com>
2026-06-21 22:50:15 +0530

    feat: add ThemeToggle component to App for theme switching

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { RouterProvider } from 'react-router';
 import router from './app.routes';
+import ThemeToggle from './ThemeToggle';
 
 const App = () => {
   return (
     <>
+      <ThemeToggle />
       <RouterProvider router={router} />
     </>
   )
