e5f77ba1e22f01fd7df48a67cb330d28c1b07a2b
Sameer <sameer.dev106@gmail.com>
2026-06-22 14:19:02 +0530

    feat: comment out BACKEND_URL variable in auth.api.js

--- Frontend/src/features/service/auth.api.js
+++ Frontend/src/features/service/auth.api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const API = axios.create({
-    baseURL: `${BACKEND_URL}/api/auth`,
+    baseURL: "/api/auth",
     withCredentials: true,
 });
 
