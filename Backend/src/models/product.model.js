e7f62147bad47e6c4f505bcb639d81dfb18a5047
Sameer <sameer.dev106@gmail.com>
2026-06-23 18:12:51 +0530

    feat: create product model with schema for title, description, seller, price, and images

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    price: {
+        amount: { type: Number, required: true },
+        currency: {
+            type: String,
+            enum: ["INR", "USD", "EUR", "GBP", "JPY"],
+            default: "INR"
+        }
+    },
+    images: [{ url: { type: String, required: true } }]
+}, { timestamps: true });
+
+const productModel = mongoose.model("product", productSchema);
+
+export default productModel;
