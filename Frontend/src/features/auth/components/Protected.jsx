87e4ef5c25ab4233af29372d91c3386c7f4af58a
Sameer <sameer.dev106@gmail.com>
2026-06-24 19:50:07 +0530

    feat: add Protected component for route protection based on user role

---
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+
+const Protected = ({ children, role = "buyer" }) => {
+  const user = useSelector((state) => state.auth.user);
+  const loading = useSelector((state) => state.auth.loading);
+
+  if (loading) return <dev>Loading...</dev>;
+  if (!user) return <Navigate to="/login" />;
+  if (user.role !== role) return <Navigate to="/" />;
+
+  return children;
+};
+
+export default Protected;
