ff3d5c271e055b6a7106fdc115573969efd47800
Sameer <sameer.dev106@gmail.com>
2026-06-24 14:24:15 +0530

    feat: add authenticateUser middleware for user authentication

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -2,6 +2,20 @@ import userModel from "../models/user.model.js";
 import { config } from "../config/config.js";
 import jwt from "jsonwebtoken";
 
+export const authenticateUser = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    try {
+        const decoded = jwt.verify(token, config.JWT_SECRET);
+        const user = await userModel.findById(decoded.id);
+        req.user = user;
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+}
+
 export const authenticateSeller = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ message: "Unauthorized" });
