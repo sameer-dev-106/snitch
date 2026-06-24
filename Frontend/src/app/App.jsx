eaaff12b522ce076f8b9ffbc9bc93823c0d9b504
Sameer <sameer.dev106@gmail.com>
2026-06-24 19:35:43 +0530

    refactor: clean up imports and ensure handleGetMe is called on component mount

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,15 +1,23 @@
-import './App.css';
-import { RouterProvider } from 'react-router';
-import router from './app.routes';
-import ThemeToggle from './ThemeToggle';
+import "./App.css";
+import { RouterProvider } from "react-router";
+import router from "./app.routes";
+import ThemeToggle from "./ThemeToggle";
+import { useAuth } from "../features/auth/hook/useAuth";
+import { useEffect } from "react";
 
 const App = () => {
+  const { handleGetMe } = useAuth();
+
+  useEffect(() => {
+    handleGetMe();
+  }, []);
+
   return (
     <>
       <ThemeToggle />
       <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
