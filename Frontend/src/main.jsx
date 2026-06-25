2165212fddf9f007616e9d2280d95f80f8f40571
Sameer <sameer.dev106@gmail.com>
2026-06-25 11:11:59 +0530

    fix: update import path for ThemeProvider in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/app.store.js";
-import { ThemeProvider } from "./app/useTheme.jsx";
+import { ThemeProvider } from "./shared/useTheme.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
