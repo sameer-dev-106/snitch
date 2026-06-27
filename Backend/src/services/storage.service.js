7f3d8991e48c09913c3cc3e3b546b9b46213fdc2
Sameer <sameer.dev106@gmail.com>
2026-06-27 22:04:11 +0530

    refactor: simplify uploadFile function by condensing upload parameters

--- Backend/src/services/storage.service.js
+++ Backend/src/services/storage.service.js
@@ -4,10 +4,6 @@ import { config } from "../config/config.js"
 const client = new imagekit({ privateKey: config.IMAGEKIT_PRIVATE_KEY });
 
 export const uploadFile = async ({ buffer, fileName, folder = "snitch" }) => {
-    const result = await client.files.upload({
-        file: await ImageKit.toFile(buffer),
-        fileName,
-        folder
-    });
+    const result = await client.files.upload({ file: await ImageKit.toFile(buffer), fileName, folder });
     return result;
 }
