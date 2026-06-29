1d186ad6622b0fc1549ebc82e131c21ea473ac2b
Sameer <sameer.dev106@gmail.com>
2026-06-29 21:32:33 +0530

    fix: add optional chaining to response data in API functions

--- Frontend/src/features/products/service/product.api.js
+++ Frontend/src/features/products/service/product.api.js
@@ -12,7 +12,7 @@ const handleApiError = (err, fallbackMessage) => {
 export const createProductApi = async (formData) => {
     try {
         const response = await API.post("/", formData);
-        return response.data;
+        return response?.data;
     } catch (err) {
         handleApiError(err, "Failed to create product");
     }
@@ -21,8 +21,17 @@ export const createProductApi = async (formData) => {
 export const getSellerProduct = async () => {
     try {
         const response = await API.get("/seller");
-        return response.data;
+        return response?.data;
     } catch (err) {
         handleApiError(err, "Failed to fetch seller products");
+    }
+}
+
+export const getAllProducts = async () => {
+    try {
+        const response = await API.get("/");
+        return response?.data;
+    } catch (err) {
+        handleApiError(err, "Filed to fetch products");
     }
 }
