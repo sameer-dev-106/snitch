0531c736631a4c013f23d2c8e4aad74fb7a5c463
Sameer <sameer.dev106@gmail.com>
2026-07-08 17:02:58 +0530

    feat: add functionality to fetch product details and add product variants

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -45,11 +45,45 @@ export const getAllProduct = async (req, res, next) => {
 
 export const getProductDetails = async (req, res, next) => {
     try {
-        const { id } = req.params
+        const { id } = req.params;
         const product = await productModel.findById(id);
         if (!product) return res.status(404).json({ message: "Product not found", success: false });
         return res.status(201).json({ message: "Product detail fetched successfully", success: true, product });
     } catch (err) {
         next(err)
+    }
+}
+
+export const addProductVariants = async (req, res, next) => {
+    try {
+        const { productId } = req.params;
+        const product = await productModel.findOne({ _id: productId, seller: req.user._id });
+        if (!product) return res.status(404).json({ message: "Product not found", success: false });
+        const files = req.files;
+        const images = [];
+        if (files || files.length !== 0) {
+            (await Promise.all(files.map(async (file) => {
+                const image = await uploadFile({ buffer: file.buffer, fileName: file.originalname })
+                return image
+            }))).map(image => images.push(image));
+        }
+        const price = req.body.priceAmount;
+        const stock = req.body.stock;
+        const attributes = JSON.parse(req.body.attributes || {});
+
+        product.variants.push({
+            images,
+            stock,
+            attributes,
+            price: {
+                amount: Number(price) || product.price.amount,
+                currency: req.body.priceCurrency || product.price.currency
+            }
+        });
+
+        await product.save();
+        return res.status(200).json({ message: "Product variant added successfully.", success: true, product });
+    } catch (err) {
+        next(err);
     }
 }
