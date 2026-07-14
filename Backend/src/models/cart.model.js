2cf0d7e477879134f6e5715b0a82c0500e1047ee
Sameer <sameer.dev106@gmail.com>
2026-07-14 22:24:48 +0530

    feat: implement cart model with schema for user cart management

---
+++ Backend/src/models/cart.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import priceSchema from "./price.schema.js";
+
+const cartSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    items: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
+        variant: { type: mongoose.Schema.Types.ObjectId, ref: "variant" },
+        quantity: { type: Number, default: 1 },
+        price: { type: priceSchema, required: true }
+    }]
+}, { timestamps: true });
+
+const cartModel = mongoose.model("cart", cartSchema);
+
+export default cartModel;
