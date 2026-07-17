5e33415560e8e71573d7a7b54feaff576e2bd064
Sameer <sameer.dev106@gmail.com>
2026-07-17 15:47:02 +0530

    fix: improve error handling and cart initialization in addToCart function

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -7,9 +7,14 @@ export const addToCart = async (req, res, next) => {
         const { productId, variantsId } = req.params;
         const { quantity = 1 } = req.body;
         const product = await productModel.findOne({ _id: productId, "variants._id": variantsId });
-        if (!product) return res.status(404).json({ message: "Product or variant not found", success: false });
+        if (!product){
+            return res.status(404).json({ message: "Product or variant not found", success: false });
+        }
         const stock = await stockOfVariant(productId, variantsId);
-        const cart = (await cartModel.findOne({ user: req.user._id })) || await cartModel.create({ user: req.user._id });
+        let cart = await cartModel.findOne({ user: req.user._id })
+        if (!cart) {
+            cart = await cartModel.create({ user: req.user._id });
+        }
         const isProductAlreadyInCart = cart.items.some(item => item.product.toString() === productId && item.variant.toString() === variantsId);
         if (isProductAlreadyInCart) {
             const quantityInCart = cart.items.find(item => item.product.toString() === productId && item.variant?.toString() === variantsId()).quantity;
