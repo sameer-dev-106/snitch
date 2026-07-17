a6a066d70054c74015d2c5df2f2afe37252f4813
Sameer <sameer.dev106@gmail.com>
2026-07-17 13:31:24 +0530

    feat: add cart routes for adding items to the cart with authentication and validation

---
+++ Backend/src/routes/cart.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
+import { validateAddToCart } from "../middlewares/cart.middleware.js";
+import { addToCart } from "../controllers/cart.controller.js";
+
+const router = Router();
+
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ * @desc Add item to cart
+ * @access Private
+ * @argument productId - ID of the product to add
+ * @argument variantId - ID of the variant to add
+ * @argument quantity - Quantity of the item to add (optional, default is 1)
+ */
+router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart);
+
+export default router;
