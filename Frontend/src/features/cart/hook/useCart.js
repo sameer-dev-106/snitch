5de3b86c4a33df61bc09f500ad08de51e13c434c
Sameer <sameer.dev106@gmail.com>
2026-07-18 11:18:47 +0530

    feat: add useCart hook for managing cart item additions

---
+++ Frontend/src/features/cart/hook/useCart.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { addItem, setError } from "../state/cart.slice";
+import { addItemApi } from "../service/cart.api";
+
+export const useCart = () => {
+    const dispatch = useDispatch();
+
+    const handleAddItem = async ({ productId, variantId }) => {
+        try {
+            const data = await addItemApi({ productId, variantId });
+            const item = data?.item;
+            if (item) dispatch(addItem(item));
+            return { success: true, item }
+        } catch (err) {
+            dispatch(setError(err?.message || "Error creating product"));
+            return { success: false, error: err };
+        }
+    }
+
+    return { handleAddItem };
+};
