5a52f0eb6ed3813d98689fff96d31b492eae386c
Sameer <sameer.dev106@gmail.com>
2026-09-02 18:33:58 +0530

    fix: add increment item quantity handler to cart with stock validation

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -53,9 +53,35 @@ export const getCart = async (req, res, next) => {
     }
 }
 
+export const incrementCartItemQuantity = async (req, res, next) => {
+    try {
+        const { productId, variantId } = req.params;
+        const product = await productModel.findOne({ _id: productId, "variants._id": variantId });
+        if (!product) return res.status(404).json({ message: "Product or variant not found", success: false });
+        const cart = await cartModel.findOne({ user: req.user._id });
+        if (cart) return res.status(404).json({ message: "Cart not found", success: false });
+        const stock = await stockOfVariant(productId, variantId);
+        const itemQuantityInCart = cart.items.find(item => item.product.toString() === productId && item.variant?.toString() === variantId)?.quantity || 0;
+        if (itemQuantityInCart + 1 > stock) {
+            return res.status(400).json({
+                message: `Only ${stock} items left in stock. and you already have ${itemQuantityInCart} items in your cart.`,
+                success: false
+            });
+        }
+        await cartModel.findOneAndUpdate(
+            { user: req.user._id, "items.product": productId, "items.variant": variantId },
+            { $inc: { "items.$.quantity": 1 } },
+            { new: true }
+        );
+        return res.status(200).json({ message: "Cart item quantity incremented successfully", success: true });
+    } catch (err) {
+        next(err)
+    }
+}
+
 export const removeFromCart = async (req, res, next) => {
     try {
-        const { productId, variantId} = req.params;
+        const { productId, variantId } = req.params;
         const cart = await cartModel.findOne({ user: req.user._id });
         if (!cart) return res.status(404).json({ message: "Cart not found", success: false });
         const itemIndex = cart.items.findIndex(item => item.product?.toString() === productId && item.variant?.toString() === variantId);
