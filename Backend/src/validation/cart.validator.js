5ce1f7f37157d0cfa3f5a3d3c81cefccf92c8b37
Sameer <sameer.dev106@gmail.com>
2026-09-03 12:02:53 +0530

    fix: add validation for decrementing cart item quantity

--- Backend/src/validation/cart.validator.js
+++ Backend/src/validation/cart.validator.js
@@ -20,3 +20,9 @@ export const validateIncrementCartItemQuantity = [
     param("variantId").optional().isMongoId().withMessage("Invalid variant ID"),
     validateRequest
 ]
+
+export const validateDecrementCartItemQuantity = [
+    param("productId").isMongoId().withMessage("Invalid product ID"),
+    param("variantId").optional().isMongoId().withMessage("Invalid variant ID"),
+    validateRequest
+]
